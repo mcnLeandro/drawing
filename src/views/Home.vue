00208c3d0bc7453a6d5173072db2fbe304aaca9d
Leandro <leandro83g.development@gmail.com>
2022-01-05 21:10:06 +0900

    feat: simple draw with conva

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,13 +14,14 @@
 </style>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from "vue";
 import { computed, defineComponent, ref, onMounted } from "vue";
 import { useStore } from "@/store";
 import MutationTypes from "@/store/mutationTypes";
 
 import SideBar from '@/components/SideBar.vue'
 import Konva from 'konva'
+import { Vector2d } from "konva/lib/types";
+
 export default defineComponent({
   name: "Home",
   components: {
@@ -49,6 +50,46 @@ export default defineComponent({
         strokeWidth: 4,
         draggable: true,
       });
+
+      layer.add(rect)
+
+      let isPaint = false;
+      let mode = "brush"
+      let lastLine: any;
+      stage.on('mousedown touchstart', function (e) {
+        isPaint = true;
+        const pos: Vector2d = stage.getPointerPosition()!;
+        lastLine = new Konva.Line({
+          stroke: '#df4b26',
+          strokeWidth: 5,
+          globalCompositeOperation:
+            mode === 'brush' ? 'source-over' : 'destination-out',
+          // round cap for smoother lines
+          lineCap: 'round',
+          // add point twice, so we have some drawings even on a simple click
+          points: [pos.x, pos.y, pos.x, pos.y],
+        });
+        layer.add(lastLine);
+      });
+
+      stage.on('mouseup touchend', function () {
+        isPaint = false;
+      });
+
+      // and core function - drawing
+      stage.on('mousemove touchmove', function (e) {
+        if (!isPaint) {
+          return;
+        }
+
+        // prevent scrolling on touch devices
+        e.evt.preventDefault();
+
+        const pos = stage.getPointerPosition()!;
+        var newPoints = lastLine.points().concat([pos.x, pos.y]);
+        lastLine.points(newPoints);
+      });
+
     })
     const form = ref("");
 
